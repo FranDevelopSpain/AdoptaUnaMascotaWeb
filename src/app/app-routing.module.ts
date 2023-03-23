14de6d61f6607ac4ac68cc6ac2a94d498f161537
Raquel <rakel_alvarez_23@yahoo.es>
2023-03-23 11:58:18 +0100

    haciendo la navegacion del navBar con las páginas y poniendo cada componente en una pestaña de la navegacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,48 @@
+import { ContactFormComponent } from './Pages/contact-form/contact-form.component';
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './Pages/about-us/about-us.component';
 import { ContactUsComponent } from './Pages/ContactUs/contact-us.component';
 import { HomeComponent } from './Pages/home/home.component';
+import { LandingPageComponent } from './Pages/landing-page/landing-page.component';
 
 const routes: Routes = [
 
-  {
+  {//Página que se cargará por defecto si no se especifica ruta
     path: "",
-    redirectTo: "home",
+    redirectTo: "landingPage",
     pathMatch: "full"
-  },
-  {
-    path: "home",
-    component: HomeComponent
-  },
-  {
-    path: "contactUs",
-    component: ContactUsComponent
   },
 
   {
-    path: "aboutUs",
-    component: AboutUsComponent
+    path: "landingPage",
+    component: LandingPageComponent,
+    children: [
+      {
+        path: "",
+        component: HomeComponent
+      },
+
+      {
+        path: "home",
+        component: HomeComponent
+      },
+      {
+        path: "contactUs",
+        component: ContactUsComponent
+      },
+    
+      {
+        path: "aboutUs",
+        component: AboutUsComponent
+      },
+
+      {
+        path: "formularioAdopcion",
+        component: ContactFormComponent
+      }
+    ]
   }
 ];
 
