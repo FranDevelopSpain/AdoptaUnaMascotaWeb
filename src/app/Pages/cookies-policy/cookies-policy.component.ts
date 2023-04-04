2779f30e11128931ee5d6189967e059290cebe5c
Paula Muerte Fernández <pmuerte98@gmail.com>
2023-04-04 20:57:14 +0200

    error corregido

--- src/app/Pages/cookies-policy/cookies-policy.component.ts
+++ src/app/Pages/cookies-policy/cookies-policy.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'app-cookies-policy',
     templateUrl: './cookies-policy.component.html',
-    styleUrls: ['./cookies-policy.component.scss']
+    styleUrls: ['./cookies-policy.component.css']
 })
 export class CookiesPolicyComponent implements OnInit {
     constructor() { }
